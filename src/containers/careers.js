f70727af21ba70daf7c515a1e98027054add0845
Shankar <shankar21mail@gmail.com>
2018-12-17 12:36:49 +0530

    feat: Update career page to include network engineer role.

--- src/containers/careers.js
+++ src/containers/careers.js
@@ -3,57 +3,8 @@ import { withSiteData } from 'react-static'
 //
 import CONST from '../constants';
 import Base from './partials/base_wrapper';
-
-class Roles extends React.Component {
-  constructor() {
-    super();
-    this.onClickRole = this.onClickRole.bind(this);
-  }
-
-  onClickRole(id) {
-    location.replace(`#${id}`)
-  }
-
-  render() {
-    const { data } = this.props;
-    const { title, desc, details } = data;
-    const id = title ? title.toLocaleLowerCase().replace(/\W/g, '_') : ``;
-    const self = this;
-    return (
-      <div className="list-content type-5">
-        <div className="list-content-b">
-          <div className="tbl-view">
-            <div className="tbl-c" id={id}>
-              <h3 className="title"><a onClick={e => {
-                e.preventDefault();
-                self.onClickRole(id);
-              }} href={`#${id}`}>{title}</a></h3>
-              <div className='tbl-cnt'>
-                <p className="p desc">{desc}</p>
-                {details && details.length > 0 ? details.map((detail, i) => (
-                  <div key={`career-detail-${i}`}>
-                    <h4 className="sub-title">{detail.head}</h4>
-                    {detail['sub-head'] ? (<h4 className="sub-title-i">{detail['sub-head']}</h4>) : null}
-                    <ul className="list bullet-5">
-                      {
-                        detail.list ? detail.list.map((ls, j) => (
-                          <li key={`details-list-${j}`}>{ls}</li>
-                        )) : null
-                      }
-                    </ul>
-                  </div>
-                )) : null}
-                <div className="btn-b">
-                  <a href={`mailto:careers@maidsafe.net?subject=${encodeURIComponent(title)}`} className="al-btn invert">Apply for this role</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+import Role from './partials/career_role';
+import careerContent from './contents/career.content'
 
 class Careers extends React.Component {
   constructor() {
@@ -93,7 +44,7 @@ class Careers extends React.Component {
       hugely positive impact on the world. If you would like to help, please
       send your CV along with a covering letter detailing why you’re a great
       ﬁt for the position to: <a href="mailto:careers@maidsafe.net">careers@maidsafe.net</a> and we will get back to you
-      as soon as we can.
+            as soon as we can.
     </p>
         </div>
 
@@ -109,86 +60,9 @@ class Careers extends React.Component {
             </ul>
           </div>
         </div>
-
-        <Roles data={{
-          title: `Software Engineer`,
-          desc: `This is an exciting yet demanding role, based either remotely or at MaidSafe’s Scottish HQ, working within the global technology sector and specifically well suited to individuals passionate and knowledgeable about autonomous networks, blockchain, distributed computing, decentralisation and cryptocurrencies.`,
-          details: [
-            {
-              head: `Role and Responsibilities`,
-              list: [
-                `Implementing the code as per the network design`,
-                `Participate in online technical discussions on Slack and and on the community run forum`,
-                `Aid the production of clear and user-focussed documentation`,
-                `Work as part of a team and participate in daily stand-ups`,
-                `Participate in testing and integration providing feedback, when required`,
-                `Maintain an excellent knowledge of the languages, tools and methodologies used by the company`,
-                `Participate in the continuous improvement and evolution of the standards and best practises`,
-                `Work independently, handle multiple tasks simultaneously and adapt quickly to change`,
-              ]
-            }, {
-              head: `Essential Requirements`,
-              list: [
-                `Experience programming in any systems level language e.g. C, C++, Rust.`,
-                `3 + years commercial experience of working to tight deadlines as part of a team.`,
-                `Experience in delivering APIs`,
-                `Experience working with distributed technology such as Distributed Hash Tables (DHTs), and/or P2P networks`,
-                `Skilled in producing clear and user-focused documentation.`,
-                `Working independently and as part of a team`,
-                `Excellent communication skills (we are a geographically distributed team)`
-              ]
-            }, {
-              head: `Desirable Skills:`,
-              'sub-head': `(These are nice-to-haves - don’t be put oﬀ applying!)`,
-              list: [
-                `Experience working within iterative Agile development process.`,
-                `Commits have been made to veriﬁable open source repositories.`,
-                `Experience using Rust is a bonus.`
-              ]
-            }
-          ]
-        }} />
-
-        <Roles data={{
-          title: `Rust Engineer`,
-          desc: `This is an exciting yet demanding role, based either remotely or at MaidSafe’s Scottish HQ, working within the global technology sector and specifically well suited to individuals passionate and knowledgeable about autonomous networks, blockchain, distributed computing, decentralisation and cryptocurrencies`,
-          details: [
-            {
-              head: `Role and Responsibilities`,
-              list: [
-                `Implementing the code as per the network design`,
-                `Participate in online technical discussions on Slack and and on the community run forum`,
-                `Aid the production of clear and user-focussed documentation`,
-                `Work as part of a team and participate in daily stand-ups`,
-                `Participate in testing and integration providing feedback, when required`,
-                `Maintain an excellent knowledge of the languages, tools and methodologies used by the company`,
-                `Participate in the continuous improvement and evolution of the standards and best practises`,
-                `Work independently, handle multiple tasks simultaneously and adapt quickly to change`,
-              ]
-            }, {
-              head: `Essential Requirements`,
-              list: [
-                `Experience of programming in Rust (preferably 1 year minimum)`,
-                `Commercial experience of working to tight deadlines`,
-                `Possess good analytical, problem solving and debugging skills`,
-                `Skilled in producing clear and focussed documentation`,
-                `Working independently and as part of a team`,
-                `Excellent communication skills (we are a geographically distributed team)`,
-              ]
-            }, {
-              head: `Desirable Skills:`,
-              'sub-head': `(These are nice-to-haves - don’t be put oﬀ applying!)`,
-              list: [
-                `Working within a remote team`,
-                `Experience of working with distributed networks`,
-                `Experience working within Interactive Agile development process`,
-                `Commits have been made to verifiable open source repositories`,
-                `Experience in either Ruby, C# or Java (in addition to Rust)`,
-              ]
-            }
-          ]
-        }} />
-
+        {
+          careerContent.length !== 0 ? careerContent.map(careerData => <Role data={careerData} />) : null
+        }
       </div>
     );
   }
