986598d4d5230c96a10e925d946cae1aa4631c3f
Shankar <shankar21mail@gmail.com>
2019-02-14 17:18:14 +0530

    Fix: Update career page

--- src/containers/contents/career.content.js
+++ src/containers/contents/career.content.js
@@ -1,137 +1,46 @@
 // Rust Engineer role
-const rustEngineer = {
-  title: 'Rust Engineer',
-  desc: 'This is an exciting yet demanding role, based either remotely or at MaidSafe’s Scottish HQ, working within the global technology sector and specifically well suited to individuals passionate and knowledgeable about autonomous networks, blockchain, distributed computing, decentralisation and cryptocurrencies',
-  details: [
-    {
-      head: `Role and Responsibilities`,
-      list: [
-        `Implementing the code as per the network design`,
-        `Participate in online technical discussions on Slack and and on the community run forum`,
-        `Aid the production of clear and user-focussed documentation`,
-        `Work as part of a team and participate in daily stand-ups`,
-        `Participate in testing and integration providing feedback, when required`,
-        `Maintain an excellent knowledge of the languages, tools and methodologies used by the company`,
-        `Participate in the continuous improvement and evolution of the standards and best practises`,
-        `Work independently, handle multiple tasks simultaneously and adapt quickly to change`,
-      ]
-    }, {
-      head: `Essential Requirements`,
-      list: [
-        `Experience of programming in Rust (preferably 1 year minimum)`,
-        `Commercial experience of working to tight deadlines`,
-        `Possess good analytical, problem solving and debugging skills`,
-        `Skilled in producing clear and focussed documentation`,
-        `Working independently and as part of a team`,
-        `Excellent communication skills (we are a geographically distributed team)`,
-      ]
-    }, {
-      head: `Desirable Skills:`,
-      'sub-head': `(These are nice-to-haves - don’t be put oﬀ applying!)`,
-      list: [
-        `Working within a remote team`,
-        `Experience of working with distributed networks`,
-        `Experience working within Interactive Agile development process`,
-        `Commits have been made to verifiable open source repositories`,
-        `Experience in either Ruby, C# or Java (in addition to Rust)`,
-      ]
-    },
-  ],
-}
-
-// Software engineer role
-const softwareEngineer = {
-  title: 'Software Engineer',
-  desc: 'This is an exciting yet demanding role, based either remotely or at MaidSafe’s Scottish HQ, working within the global technology sector and specifically well suited to individuals passionate and knowledgeable about autonomous networks, blockchain, distributed computing, decentralisation and cryptocurrencies.',
-  details: [
-    {
-      head: `Role and Responsibilities`,
-      list: [
-        `Implementing the code as per the network design`,
-        `Participate in online technical discussions on Slack and and on the community run forum`,
-        `Aid the production of clear and user-focussed documentation`,
-        `Work as part of a team and participate in daily stand-ups`,
-        `Participate in testing and integration providing feedback, when required`,
-        `Maintain an excellent knowledge of the languages, tools and methodologies used by the company`,
-        `Participate in the continuous improvement and evolution of the standards and best practises`,
-        `Work independently, handle multiple tasks simultaneously and adapt quickly to change`,
-      ],
-    }, {
-      head: `Essential Requirements`,
-      list: [
-        `Experience programming in any systems level language e.g. C, C++, Rust.`,
-        `3 + years commercial experience of working to tight deadlines as part of a team.`,
-        `Experience in delivering APIs`,
-        `Experience working with distributed technology such as Distributed Hash Tables (DHTs), and/or P2P networks`,
-        `Skilled in producing clear and user-focused documentation.`,
-        `Working independently and as part of a team`,
-        `Excellent communication skills (we are a geographically distributed team)`
-      ],
-    }, {
-      head: `Desirable Skills:`,
-      'sub-head': `(These are nice-to-haves - don’t be put oﬀ applying!)`,
-      list: [
-        `Experience working within iterative Agile development process.`,
-        `Commits have been made to veriﬁable open source repositories.`,
-        `Experience using Rust is a bonus.`
-      ],
-    },
-  ],
-}
-
-// Networking Engineer role
-const networkEngineer = {
-  title: 'Networking Engineer',
-  desc: 'This is an exciting yet demanding role, based either remotely or at MaidSafe’s Scottish HQ, working within the global technology sector and specifically well suited to individuals passionate and knowledgeable about autonomous networks, blockchain, distributed computing, decentralisation and cryptocurrencies.',
-  details: [
-    {
-      head: `Role and Responsibilities`,
-      list: [
-        `Implementing the code as per the network design`,
-        `Participate in online technical discussions on Slack and on the community run forum`,
-        `Aid the production of clear and user-focussed documentation`,
-        `Work as part of a team and participate in daily stand-ups`,
-        `Participate in testing and integration providing feedback, when required`,
-        `Maintain an excellent knowledge of the languages, tools and methodologies used by the company`,
-        `Participate in the continuous improvement and evolution of the standards and best practises`,
-        `Work independently, handle multiple tasks simultaneously and adapt quickly to change`,
-
-      ],
-    }, {
-      head: `Essential skills required`,
-      list: [
-        `Expert in Network Programming, specifically involving async/non-blocking paradigms`,
-        `Have experience of NAT traversal and know as much about the network-concepts as possible`,
-        `Have knowledge of modern rust paradigm of tokio/futures`,
-        `Have good background in working with network libraries esp in Rust`,
-        `Have detailed knowledge of TCP/UDP `,
-        `Experience of programming in Rust (preferably 1 year minimum)`,
-        `Commercial experience of working to tight deadlines`,
-        `Possess good analytical, problem solving and debugging skills`,
-        `Skilled in producing clear and focussed documentation`,
-        `Working independently and as part of a team`,
-        `Excellent communication skills (we are a geographically distributed team)`,
-
-      ],
-    }, {
-      head: `Desirable Skills:`,
-      'sub-head': `(These are nice-to-haves - don’t be put oﬀ applying!)`,
-      list: [
-        `Working within a remote team`,
-        `Experience of working with distributed networks`,
-        `Experience working within Interactive Agile development process`,
-        `Commits have been made to verifiable open source repositories`,
-        `Experience in either Ruby, C# or Java (in addition to Rust)`,
-
-      ],
-    },
-  ],
-}
+// const rustEngineer = {
+//   title: 'Rust Engineer',
+//   desc: 'This is an exciting yet demanding role, based either remotely or at MaidSafe’s Scottish HQ, working within the global technology sector and specifically well suited to individuals passionate and knowledgeable about autonomous networks, blockchain, distributed computing, decentralisation and cryptocurrencies',
+//   details: [
+//     {
+//       head: `Role and Responsibilities`,
+//       list: [
+//         `Implementing the code as per the network design`,
+//         `Participate in online technical discussions on Slack and and on the community run forum`,
+//         `Aid the production of clear and user-focussed documentation`,
+//         `Work as part of a team and participate in daily stand-ups`,
+//         `Participate in testing and integration providing feedback, when required`,
+//         `Maintain an excellent knowledge of the languages, tools and methodologies used by the company`,
+//         `Participate in the continuous improvement and evolution of the standards and best practises`,
+//         `Work independently, handle multiple tasks simultaneously and adapt quickly to change`,
+//       ]
+//     }, {
+//       head: `Essential Requirements`,
+//       list: [
+//         `Experience of programming in Rust (preferably 1 year minimum)`,
+//         `Commercial experience of working to tight deadlines`,
+//         `Possess good analytical, problem solving and debugging skills`,
+//         `Skilled in producing clear and focussed documentation`,
+//         `Working independently and as part of a team`,
+//         `Excellent communication skills (we are a geographically distributed team)`,
+//       ]
+//     }, {
+//       head: `Desirable Skills:`,
+//       'sub-head': `(These are nice-to-haves - don’t be put oﬀ applying!)`,
+//       list: [
+//         `Working within a remote team`,
+//         `Experience of working with distributed networks`,
+//         `Experience working within Interactive Agile development process`,
+//         `Commits have been made to verifiable open source repositories`,
+//         `Experience in either Ruby, C# or Java (in addition to Rust)`,
+//       ]
+//     },
+//   ],
+// }
 
 const roles = []
-roles.push(
-  softwareEngineer,
-  rustEngineer,
-  networkEngineer,
-)
+// roles.push(
+//   rustEngineer,
+// )
 export default roles
