2a36f180c8f1f24d0055e6cda81592b62e3c4085
Francis Brunelle <frabrunelle@gmail.com>
2019-11-11 13:24:41 -0500

    link to the events page on the forum

--- src/constants.js
+++ src/constants.js
@@ -24,7 +24,7 @@ export default {
       youTube: 'https://www.youtube.com/channel/UChDck5R_C9i6XTrS66tbwOw',
       reddit: 'https://www.reddit.com/r/safenetwork/',
       twitter: 'https://twitter.com/maidsafe',
-      meetup: 'https://safenetwork.org/meetup-groups/',
+      meetup: 'https://safenetforum.org/t/safe-network-upcoming-events/29029',
       telegram: 'https://t.me/safenetwork',
       facebook: 'https://www.facebook.com/GetMaidSafe/',
       medium: 'https://medium.com/safenetwork',
