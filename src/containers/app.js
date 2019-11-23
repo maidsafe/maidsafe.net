44973912aa55a282468c5ae40aaf718e023ec9f5
Francis Brunelle <frabrunelle@gmail.com>
2019-11-22 21:38:00 -0500

    add scroll restoration

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Root, Routes } from 'react-static'
+import { useHistory } from 'react-router-dom'
 import { hot } from 'react-hot-loader'
 //
 import '../sass/main.sass';
 
+const ScrollRestoration = () => {
+  const history = useHistory()
+  useEffect(() => {
+    if (history.action !== 'POP') {
+      window.scrollTo(0, 0)
+    }
+  }, [history.location.pathname])
+  return null
+}
+
 const App = () => (
   <Root>
-    <div className="root-b">
-      <React.Suspense fallback={<span>Loading...</span>}>
-        <Routes />
-      </React.Suspense>
-    </div>
+    <React.Suspense fallback={<span>Loading...</span>}>
+      <ScrollRestoration />
+      <Routes />
+    </React.Suspense>
   </Root>
 )
 
